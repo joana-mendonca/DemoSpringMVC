9da3a2da0f82acf174a72c446ad0b62b00c89787
J04N4 <joanaqmendonca@hotmail.com>
2024-03-30 16:08:51 +0000

    Lesson_4-Thymeleaf_e_diferentes_formas_de_renderizar_paginas_no_controller

--- demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -10,5 +13,23 @@ public class HelloController {
     public String hello() {
         /*O spring vai renderizar o ficheiro (a "view") que esta em resources/templates/hello.html*/
         return "hello";
+    }
+    @GetMapping("/hello-servlet")
+    public String helloServlet(HttpServletRequest request) {
+        request.setAttribute("nome", "HttpServletRequest");
+        /*O spring vai renderizar o ficheiro (a "view") que esta em resources/templates/hello.html*/
+        return "hello";
+    }
+    @GetMapping("/hello-model")
+    public String helloModel(Model model) {
+        model.addAttribute("nome", "Model");
+        /*O spring vai renderizar o ficheiro (a "view") que esta em resources/templates/hello.html*/
+        return "hello";
+    }
+    @GetMapping("/hello-model-and-view")
+    public ModelAndView helloModelAndView() {
+        ModelAndView mv = new ModelAndView("hello"); //nome do ficheiro html a ser renderizado
+        mv.addObject("nome", "Model And View");
+        return mv;
     }
 }
